e8e46be2a293ffe6d826bacaf49aa93e7be60643
meep-morp <gracieaerogirl@gmail.com>
2020-06-11 14:59:29 -0500

    Added create react app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+import { ProductProvider } from "./contexts/ProductContext";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
-
 	const addItem = item => {
 		// add the given item to the cart
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductProvider>
+			<div className="App">
+				<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			</div>
+		</ProductProvider>
 	);
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { useContext } from "react";
 
 // Components
-import Product from './Product';
+import Product from "./Product";
+import { ProductContext } from "../contexts/ProductContext";
 
 const Products = props => {
+	const [products] = useContext(ProductContext);
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
+			{products.map(product => (
+				<Product key={product.id} product={product} addItem={props.addItem} />
 			))}
 		</div>
 	);
